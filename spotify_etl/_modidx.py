0fdb1d9d1901c7b0f0d01b2b3f7c4d789b5c6f75
Warren Hyson <84579627+LearnThinkCreate@users.noreply.github.com>
2024-04-17 21:51:25 -0400

    re-format

--- spotify_etl/_modidx.py
+++ spotify_etl/_modidx.py
@@ -5,6 +5,8 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://LearnThinkCreate.github.io',
                 'git_url': 'https://github.com/LearnThinkCreate/spotify-etl',
                 'lib_path': 'spotify_etl'},
-  'syms': { 'spotify_etl.core': { 'spotify_etl.core.clean_streaming_history': ('core.html#clean_streaming_history', 'spotify_etl/core.py'),
-                                  'spotify_etl.core.extract_streaming_history': ( 'core.html#extract_streaming_history',
-                                                                                  'spotify_etl/core.py')}}}
+  'syms': { 'spotify_etl.core': { 'spotify_etl.core.clean_streaming_history': ( 'pull_history.html#clean_streaming_history',
+                                                                                'spotify_etl/core.py'),
+                                  'spotify_etl.core.extract_streaming_history': ( 'pull_history.html#extract_streaming_history',
+                                                                                  'spotify_etl/core.py'),
+                                  'spotify_etl.core.get_pickle_data': ('pull_history.html#get_pickle_data', 'spotify_etl/core.py')}}}
